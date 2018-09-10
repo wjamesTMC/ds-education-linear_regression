f3ee9425fed5569631c7d934663f5e72090d6510
wjamesTMC <jamesw@csps.com>
2018-09-10 16:12:28 -0400

    Added libraries to the template

--- unit_1-introduction-to-regression/0-template.R
+++ unit_1-introduction-to-regression/0-template.R
@@ -5,4 +5,9 @@
 # --------------------------------------------------------------------------------
 
 # Setup
-
+library(tidyverse)
+library(dslabs)
+library(dplyr)
+library(ggplot2)
+library(Lahman)
+library(HistData)
